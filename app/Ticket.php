8b480cb74cff8bc60c76a995d54c2cf41949d0e4
sangcao <sangcao1003@gmail.com>
2018-07-13 16:19:58 +0700

    Chapter 3 - In process

---
+++ app/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+	// protected $fillable = ['title', 'content', 'slug', 'status', 'user_id'];
+	// protected $guarded = ['id'];
+	
+    protected $table='tickets';
+    protected $primaryKey='id';
+    protected $guarded=[];
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,28 @@
 Route::get('/','PagesController@home');
 Route::get('/about','PagesController@about');
 Route::get('/contact','PagesController@contact');
+
+Route::get('/contact','TicketsController@create');
+Route::post('/contact','TicketsController@store');
+Route::get('/tickets', 'TicketsController@index');
+Route::get('/ticket/{slug?}', 'TicketsController@show');
+Route::get('/ticket/{slug?}/edit','TicketsController@edit');
+Route::post('/ticket/{slug?}/edit','TicketsController@update');
+Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
+Route::get('sendemail', function () {
+
+    $data = array(
+        'name' => "Learning Laravel",
+    );
+
+    Mail::send('emails.welcome', $data, function ($message) {
+
+        $message->from('sangcao1003@gmail.com', 'Learning Laravel');
+
+        $message->to('sangcao1003@gmail.com')->subject('Learning Laravel test email');
+
+    });
+
+    return "Your email has been sent successfully";
+
+});
